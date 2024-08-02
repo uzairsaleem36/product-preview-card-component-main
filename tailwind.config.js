41e9ad82285fa0ab6e6b5bc7f54a417fb8cbb153
Uzair Saleem <uzair.aptech56@gmail.com>
2024-08-02 16:24:05 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    colors: {
+      'VeryDarkBlue': 'hsl(212, 21%, 14%)',
+      'DarkGrayishBlue': 'hsl(228, 12%, 48%)',
+      'White': 'hsl(0, 0%, 100%)',
+      'DarkCyan': 'hsl(158, 36%, 37%)',
+      'Cream': 'hsl(30, 38%, 92%)'
+    },
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        fraunces: ['Fraunces', 'serif'],
+      },
+      maxWidth:{
+        'custom': '640px',
+      }
+    },
+  },
+  plugins: [],
+}
